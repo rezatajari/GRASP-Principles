84e16532e3b4cf199dd8f08c69cd6c1e9ee0d7f2
rezatajari <reza.tajari70@gmail.com>
2025-05-06 12:52:45 +0330

    feat: information

---
+++ Information.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRASP_Principles
+{
+    internal class Information
+    {
+    }
+
+    internal class Book
+    {
+
+        static Book Create(string name, string author, string categoryName)
+        {
+            return new Book { Name = name, Author = author, CategoryName = categoryName };
+        }
+
+        private Book() { }
+        public string Name { get; private set; }
+        public string CategoryName { get; private set; }
+        public string Author { get; private set; }
+        public bool IsBarrow { get; private set; }
+
+        public bool IsAvailable()
+        {
+            return IsBarrow;
+        }
+
+        public void Barrow()
+        {
+           IsBarrow= true;
+        }
+
+        public void Return()
+        {
+            IsBarrow = false;
+        }
+
+    }
+}
